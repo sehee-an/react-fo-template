1c6c1c0c11f1660eff53dce76cada88edf11f4ff
sehee-an <an.sehee324@gmail.com>
2025-12-04 11:17:41 +0900

    chore: refine ESLint flat config and integrate Prettier setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,35 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    // 적용 파일
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+
+    // 환경설정
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+
+    // React 설정 (Hook 규칙, JSX 등)
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginReact.configs.flat.recommended,
+
+  
+  prettier, // Prettier와 ESLint 충돌 방지 (반드시 마지막!)
 ]);
