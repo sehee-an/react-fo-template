ec859247b15b4dd36a505925a61162c623b8cf80
sehee-an <an.sehee324@gmail.com>
2026-01-05 11:07:32 +0900

    feat: add Login page with responsive form layout using reusable Input and Button components

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import Button from "~/components/ui/Button";
+import Input from "~/components/ui/Input";
+
 export default function Login() {
-  return <h2 className="text-2xl font-semibold">Login Page</h2>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  function handleSubmit(e: React.FormEvent){
+    e.preventDefault();
+    console.log("Login attempt", {email, password});
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white border rounded-lg shadow-sm">
+      <h2 className="text-2xl font-semibold mb-6 text-center">Login</h2>
+
+      <form onSubmit={handleSubmit} className="space-y-5">
+        <Input
+          label="Email"
+          type="email"
+          placeholder="example@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button type="submit" fullWidth>
+          Login
+        </Button>
+      </form>
+    </div>
+  );
 }
